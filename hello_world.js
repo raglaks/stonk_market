d4731bf903ca34b51bd5d83202439d10791cb45a
Raghava Lakshminarayana <raglaks94@gmail.com>
2020-04-05 18:08:07 -0500

    add method for max val and max user

--- hello_world.js
+++ hello_world.js
@@ -13,12 +13,17 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+
   if (msg.content === 'stonk') {
 
     msg.reply(`stonks for life maddafaka`);
+
   } else if (msg.content.startsWith(price_command)) {
+
     // get the price
+    
     const intToParse = msg.content.substring(price_command.length, msg.content.len);
+    
     // parse it to an int
     const parsedInt = parseInt(intToParse);
 
@@ -27,18 +32,35 @@ client.on('message', msg => {
     if (isNaN(parsedInt)) {
       msg.reply(`Please input your turnip price as per the following example: "!price 42"`);
     } else {
-      msg.reply(`Noting your turnip price to be ${parsedInt}`);
+
+      // compararlo con el max price para ver si es mayor
+      // si es mayor, actualizar max_value y max_member
+      // (el member se obtiene con msg.member)
+      //DONE
+
+      if (parsedInt > max_value) {
+
+        max_value = parsedInt;
+
+        max_member = msg.author.username;
+
+        let max_message = `${max_member} set the new max price of ${max_value}`;
+
+        msg.reply(`Noting your turnip price to be ${parsedInt}\n${max_message}`);
+
+      } else {
+
+        msg.reply(`Noting your turnip price to be ${parsedInt}`);
+
+      }
+      
     }
 
-
-
-    // TODO: compararlo con el max price para ver si es mayor.
-
-    // TODO: si es mayor, actualizar max_value y max_member
-    // (el member se obtiene con msg.member)
   }
 
   // TODO: Agregar handling de comando !max y que regrese el precio y el nombre del member (max_member) para saber quien es
+
+  //TODO: agregar un "wipe"--resetear el max price cada dia
 
 });
 
