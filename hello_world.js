c6d4908d9b599ce04aa7ce6097d6e934e0e23ae2
Raghava Lakshminarayana <raglaks94@gmail.com>
2020-04-05 18:27:58 -0500

    add max method and comments

--- hello_world.js
+++ hello_world.js
@@ -4,9 +4,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 let max_value = 0;
-let max_member = "No one";
+let max_member = "no one";
+let max_message = "no one";
 
 const price_command = "!price";
+const max_command = "!max";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -21,40 +23,57 @@ client.on('message', msg => {
   } else if (msg.content.startsWith(price_command)) {
 
     // get the price
-    
     const intToParse = msg.content.substring(price_command.length, msg.content.len);
     
     // parse it to an int
     const parsedInt = parseInt(intToParse);
 
     // check that we did get a number
-
     if (isNaN(parsedInt)) {
+
       msg.reply(`Please input your turnip price as per the following example: "!price 42"`);
+
     } else {
 
-      // compararlo con el max price para ver si es mayor
-      // si es mayor, actualizar max_value y max_member
-      // (el member se obtiene con msg.member)
-      //DONE
-
+      // checks if current price entered is greater than max price
       if (parsedInt > max_value) {
 
+        //if so, then set new max price to current price and also set new max member to current member
         max_value = parsedInt;
 
         max_member = msg.author.username;
 
-        let max_message = `${max_member} set the new max price of ${max_value}`;
+        //string message for who's setting new max price
+        max_message = `${max_member} set the new max price of ${max_value}`;
 
+        //discord js reply method w complete message that new max price has been set by ...
         msg.reply(`Noting your turnip price to be ${parsedInt}\n${max_message}`);
 
       } else {
 
+        //simply for UI purposes--acknowledging user input
         msg.reply(`Noting your turnip price to be ${parsedInt}`);
 
       }
       
     }
+
+  } else if (msg.content.startsWith(max_command)) {
+
+    //check if max val has been set
+    //if so, then return who set it at what price
+    //if not, then tell user how to set price
+    if (max_message !== "no one") {
+
+      msg.reply(`${max_member} set the max price of ${max_value}.`);
+
+    } else {
+
+      msg.reply(`No one has set a max price yet. Please input your turnip price as per the following example: "!price 42"`)
+
+    }
+
+    
 
   }
 
